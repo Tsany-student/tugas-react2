5c5f089bd014f01ab13b53a8e8dd696e0f1759af
Tsany-student <tsani1209@student.abudzar.sch.id>
2026-01-28 13:36:46 +0700

    push ulang

--- src/Form/Counter.jsx
+++ src/Form/Counter.jsx
@@ -1,20 +1,39 @@
 import { useState } from "react";
 
-export default function Counter() {
-  let [counter, setCounter] = useState(0);
+export default function DaftarSiswa() {
+  const [list, setList] = useState([]);
+  const [nama, setNama] = useState("");
 
   return (
     <div>
+      <input
+        type="text"
+        value={nama}
+        onChange={(e) => setNama(e.target.value)}
+        placeholder="Nama siswa"
+      />
       <button
-            onClick={() => {
-            setCounter(counter + 1);
-          console.log(counter);
+        onClick={() => {
+          setList([...list, nama]);
+          setNama("");
         }}
       >
-        Increment
+        Tambah
       </button>
 
-      <h1>Counter: {counter}</h1>
+      <table border="1">
+        <tr>
+          <th>No</th>
+          <th>Nama</th>
+        </tr>
+
+        {list.map((item, index) => (
+          <tr key={index}>
+            <td>{index + 1}</td>
+            <td>{item}</td>
+          </tr>
+        ))}
+      </table>
     </div>
   );
 }
